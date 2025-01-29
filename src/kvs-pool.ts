a19bf52df60e2d893c316ae4aa1b39bea0e43b2b
Mario Zito <mario.zito@treme.io>
2025-01-29 12:13:05 -0300

    Merge pool types on kvs-pool

--- src/kvs-pool.ts
+++ src/kvs-pool.ts
@@ -3,9 +3,11 @@
  * We provide two storage types: 
  * - 'mem' a memory (non-persistent) pool, can be used in browser and Node
  * - 'lmdb' a LMDB persistent pool, can be used only in Node
- */
-import { MemKVS } from "./kvs-mem-pool.js";
-import { LmdbKVS } from "./kvs-lmdb-pool.js";
+*/
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { open } from "lmdb" ;
+import { LMDB } from "./config.js";
+import { logger } from "./logger.js";
 
 export { 
   type KVSPool, 
@@ -20,4 +22,89 @@ type KVSPoolType = 'lmdb' | 'mem';
 function openPool(type: KVSPoolType): MemKVS | LmdbKVS {
   if (type === 'lmdb') return new LmdbKVS();
   return new MemKVS(); // default 
+}
+
+/**
+ * A Memory (no-persistent) KeyValue store for saving Semaphore Groups.
+ * NOTE: works on Node and in browser.
+ * In the browser, only memory groups can be managed.
+*/
+class MemKVS {
+  private static _POOL: Map<string, any>;
+
+  public get(key: string): any | null {
+    const data = MemKVS._POOL.get(key) || null;
+    return data;
+  }
+  
+  public put(key: string, data: any) {
+    MemKVS._POOL.set(key, data);
+  }
+}
+
+/**
+ * A LMDB KeyValue store for saving Semaphore Groups.
+ * NOTE: only works on Node, not in browser.
+*/
+class LmdbKVS {
+  private static _DB: any = null;
+
+  public get(key: string): any | null {
+    const db = LmdbKVS.openDb();
+    const data = db.get(key) || null;
+    return data;
+  }
+  
+  public put(key: string, data: any) {
+    const db = LmdbKVS.openDb();
+    db.transaction(() => {
+      db.put(key, data);
+    });
+  }
+
+  private static openDb(options?: { log: any }) {
+    if (LmdbKVS._DB) return LmdbKVS._DB;
+    let logging = options?.log || logger;
+    logging.info(`Open KVStore path='${LMDB.PATH}'`);
+    try {
+      const db = open({
+        path: LMDB.PATH,
+        // any options go here
+        encoding: 'msgpack',
+        sharedStructuresKey: Symbol.for('sharedstructures'),
+        cache: true,
+        // compression: true,
+      });
+      LmdbKVS._DB = db;
+    }
+    catch (err) {
+      logging.error(err);
+      LmdbKVS._DB = null;
+      throw Error(`ERROR opening KVStore path:'${LMDB.PATH}' reason:'${err}'`);
+    }
+    return LmdbKVS._DB;
+  }
+
+  public static async find(q: string) {
+    const db = LmdbKVS.openDb();
+    if (!q) throw Error("KVS.find requires a search word.")
+    let found: any[] = [];
+    db.getRange()
+      .filter((t: any) => q && t.key.includes(q))
+      .forEach((t: any) => {
+        found.push(t)
+      })
+    return found;  
+  }
+
+  public static async browseKeys(q: string | undefined) {
+    console.log(`\n---\nBrowse LMDB`);
+    const db = LmdbKVS.openDb({ log: console });
+    console.log(`Search ${q ? `keys containing: '${q}'` : 'all keys'}`)
+    db.getRange()
+      .filter((t: any) => (q ? t.key.includes(q) : true ))
+      .forEach((t: any) => {
+        console.log(`\n${t.key}: `, JSON.stringify(t.value, null, 2));
+      })
+  }
 }
